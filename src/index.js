16e213deed72a875bc8000a7f5a5df4de5d1d128
Itoro Philip <itorophilip1998@gmail.com>
2022-08-19 15:05:46 +0100

    dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+/* eslint-disable */
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
